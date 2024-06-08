6720d4727b7889178f8dad21108df65affba00e8
andrea285 <andrea.lojpur285@gmail.com>
2024-06-08 22:00:23 +0200

    Initial project setup

---
+++ src/test/java/com/example/medicinskaOprema/MedicinskaOpremaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicinskaOprema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinskaOpremaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
