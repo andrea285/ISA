d7b3c4f62632faa87310d882f9fdc0d6da0f6999
andrea285 <andrea.lojpur285@gmail.com>
2024-06-09 01:40:25 +0200

    user registration

---
+++ src/main/java/com/example/medicinskaOprema/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.medicinskaOprema.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/example/medicinskaOprema/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.medicinskaOprema.controller;
+
+import com.example.medicinskaOprema.model.User;
+import com.example.medicinskaOprema.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+}
